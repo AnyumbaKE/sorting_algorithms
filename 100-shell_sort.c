f02fa9d132a0616558c00654683314ffbd0e5d7e
AnyumbaKE <esanyumba@gmail.com>
2023-10-20 17:03:56 -0700

    4. Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: first integer to swap.
+ * @b: second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort order using the shell sort algorithm.
+ * @array: array of integers.
+ * @size: size of array
+ * Description: Uses the Knuth interval sequence.
+ **/
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
