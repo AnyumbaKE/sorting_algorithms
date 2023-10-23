1dc50c31a353ced8d7769e2d52c3f455b66b7f53
AnyumbaKE <esanyumba@gmail.com>
2023-10-23 11:30:28 -0700

    changes

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -3,7 +3,8 @@
 #include "sort.h"
 
 void swap_ints(int *a, int *b);
-void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
 void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
 void bitonic_sort(int *array, size_t size);
 
